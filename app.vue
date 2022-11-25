0d6819f9de4c5d8059896ec5e7f55f71e7b06cf1
Jelby <evgeniy.ipatov92@mail.ru>
2022-11-25 21:03:06 +0300

    feat: add task list base design and task status change functionality

--- app.vue
+++ app.vue
@@ -6,7 +6,27 @@
       </v-container>
     </v-app-bar>
     <v-main>
-      <v-container>Контентная часть</v-container>
+      <v-container>
+        <v-tabs
+          v-model="state.activeTab"
+          bg-color="transparent"
+          color="basil"
+          grow
+        >
+          <v-tab value="completed"> Текущие задачи </v-tab>
+          <v-tab value="notCompleted"> Выполненные задачи </v-tab>
+        </v-tabs>
+
+        <v-window v-model="state.activeTab">
+          <v-window-item value="completed">
+            <task-list :tasks="notCompletedTasks" />
+          </v-window-item>
+
+          <v-window-item value="notCompleted">
+            <task-list :tasks="completedTasks" />
+          </v-window-item>
+        </v-window>
+      </v-container>
     </v-main>
     <v-footer app>
       <v-container>Подвал</v-container>
@@ -14,8 +34,36 @@
   </v-app>
 </template>
 
+<script lang="ts" setup>
+import { Ref, ComputedRef } from "vue";
+import { Task } from "@/types";
+
+// v-input-control-height Найти как поменять высоту
+
+const tasksList: Ref<Task[]> = ref([
+  { id: 1, title: "Задача 1", isCompleted: false },
+  { id: 2, title: "Задача 2", isCompleted: false },
+  { id: 3, title: "Задача 3", isCompleted: false },
+  { id: 4, title: "Задача 3", isCompleted: true },
+]);
+
+const state = reactive({
+  activeTab: null,
+});
+
+const completedTasks: ComputedRef<Task[]> = computed(() => {
+  return tasksList.value.filter((todoItem: Task) => todoItem.isCompleted);
+});
+const notCompletedTasks: ComputedRef<Task[]> = computed(() => {
+  return tasksList.value.filter((todoItem: Task) => !todoItem.isCompleted);
+});
+</script>
+
 <style lang="scss">
 .v-container {
   max-width: 960px;
+}
+.v-window {
+  padding: 1rem 0.4rem 0.4rem;
 }
 </style>
---
+++ components/TaskList.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-expansion-panels v-if="tasks.length" variant="accordion">
+    <v-expansion-panel v-for="task in tasks" :key="task.id">
+      <v-expansion-panel-title>
+        <div class="d-flex align-center">
+          <v-checkbox
+            v-model="task.isCompleted"
+            color="success"
+            hide-details
+            density="compact"
+          />
+          {{ task.title }}
+        </div>
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        {{ task.text }}
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
+  <div v-else>Данный список пуст</div>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from "vue";
+import { Task } from "@/types";
+
+defineProps({
+  tasks: {
+    type: Array as PropType<Task[]>,
+    required: true,
+    default: () => [],
+  },
+});
+</script>
