e23d4ea091cb1964c8a9c0b3f08f195f5b323786
Jelby <evgeniy.ipatov92@mail.ru>
2022-11-25 18:32:55 +0300

    feat: add base layout

--- app.vue
+++ app.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>Start</div>
+  <v-app>
+    <v-app-bar>
+      <v-container>
+        <v-app-bar-title>TODO List</v-app-bar-title>
+      </v-container>
+    </v-app-bar>
+    <v-main>
+      <v-container>Контентная часть</v-container>
+    </v-main>
+    <v-footer app>
+      <v-container>Подвал</v-container>
+    </v-footer>
+  </v-app>
 </template>
+
+<style lang="scss">
+.v-container {
+  max-width: 960px;
+}
+</style>
