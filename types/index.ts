51cd9bf4dd3487495028211a1af1ffa66375462e
Jelby <evgeniy.ipatov92@mail.ru>
2022-11-28 19:27:43 +0300

    feat: categorization of tasks

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,12 @@
 export interface Task {
   id: number;
+  category_id: number;
   title: string;
   isCompleted: boolean;
   text?: string;
+}
+
+export interface TaskCategory {
+  id: number;
+  title: string;
 }
