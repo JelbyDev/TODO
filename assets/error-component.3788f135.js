8bf314f849a70a35086b0152329b7bdadf99342b
Jelby <evgeniy.ipatov92@mail.ru>
2023-01-10 18:10:21 +0300

    Updates

---
+++ assets/error-component.3788f135.js
@@ -0,0 +1,1 @@
+import{d as r,c as e}from"./entry.d367bdac.js";const a=r({__name:"error",setup(o){return e({redirect:"/"}),()=>{}}});export{a as default};
--- assets/error-component.50da6294.js
+++
@@ -1,1 +0,0 @@
-import{d as r,c as e}from"./entry.b2e17741.js";const a=r({__name:"error",setup(o){return e({redirect:"/"}),()=>{}}});export{a as default};
